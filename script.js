94541542a9b4d5443544b9d41ec5977e8056fe96
Bj vaishnav <116649556+Bjvai@users.noreply.github.com>
2024-07-24 14:33:24 +0530

    new file

---
+++ script.js
@@ -0,0 +1,10 @@
+// script.js
+document.addEventListener("DOMContentLoaded", function() {
+    window.addEventListener("load", function() {
+        // Hide the loader
+        document.getElementById('loader').style.display = 'none';
+
+        // Show the content
+        document.getElementById('content').style.display = 'block';
+    });
+});
